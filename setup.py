32abc6588058493b9c131d023670f1ee929c6e24
JimmyAdams <jakub.adamec.svk@gmail.sk>
2020-05-27 00:16:09 +0200

    setup use with setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='PlayerClientMPD',
@@ -8,7 +8,7 @@ setup(
     author_email='xadame41@stud.fit.vutbr.cz',
     description='Player MPD',
     package_data = {"PlayerClientMPD" : ["playlists", "icons/*"]},
-    packages = ["PlayerClientMPD"],
+    packages=find_packages(),
     install_requires=['numpy >= 1.11.1',
      'PyQt5',
      'python-vlc',
